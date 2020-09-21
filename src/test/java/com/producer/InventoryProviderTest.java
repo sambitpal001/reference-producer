0c9900379c1f793dc963b471aaf4b7d48da011a9
SambitKumarPal <sambit.pal@wawa.com>
2020-09-21 09:04:42 -0400

    Update InventoryProviderTest.java

--- src/test/java/com/producer/InventoryProviderTest.java
+++ src/test/java/com/producer/InventoryProviderTest.java
@@ -25,7 +25,7 @@ import org.springframework.test.context.ActiveProfiles;
 @RunWith(SpringRestPactRunner.class)
 @SpringBootTest(classes=PactContractProviderApplication.class,properties={"spring.profiles.active=test","spring.cloud.config.enabled=false"},webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ActiveProfiles("test")
-//@PactBroker(host="localhost",port="8500")
+@PactBroker(host="localhost",port="8500")
 @Provider("inventory_provider")
 public class InventoryProviderTest {
 	
